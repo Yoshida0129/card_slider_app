73b44161313c9a4052175b08d91edd0709d416c3
Tatsuya Yoshida <alpacagame0129@gmail.com>
2019-06-20 02:43:38 +0900

    Add: header style付与

--- src/views/header.jsx
+++ src/views/header.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import '../assets/header.scss';
 
 export default class Header extends Component {
   render() {
     return (
       <header>
-        <p>CardSlider</p>
+        <p>Card Slider</p>
       </header>
     )
   }
